6d9827d3721f97c119781c45ee2bc8a88f03e1a6
JØRDAN SANTØS <jordansantos-11@outlook.com>
2022-08-15 10:55:42 -0300

    updater project-api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, {Component} from 'react'
 import Movies from './Services/Movies.js'
+
 export default class App extends Component{
+  
+  // state={
+  //   nome: 'David'
+  // }
+
+  // componentDidMount(){
+  //   document.title = this.state.nome
+  // }
+  // componentDidUpdate(state){
+  //   if(state !== this.state.nome){
+  //     document.title = this.state.nome
+  //   }
+  // }
+  // mudar = () =>{
+  //   this.setState({
+  //     nome: this.state.nome == "leonardo" ? "david" : "leonardo",
+  //     corpo: document.body.style.backgroundColor = "red"
+  //   })
+  // }
+
   render(){
     return(
       <div>
-        <h1>OLA POVO</h1>
-        <Movies.js />
+        <Movies />
       </div>
     )
   }
--- src/Services/Movies.js
+++ src/Services/Movies.js
@@ -2,14 +2,47 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 const FilmesApi = axios.create({
-    baseURL:'https://api.themoviedb.org/3/movie/popular?api_key=a993799d52868d24bfa8c0b887e82f95&language=pt-BR&page=1'
+    baseURL: 'https://api.themoviedb.org/3/movie/popular?api_key=a993799d52868d24bfa8c0b887e82f95&language=pt-BR&page=1'
 })
 console.log(FilmesApi)
 export default class Movie extends Component {
+
+    state = {
+        movies: []
+    }
+
+    componentDidMount() {
+        this.getMovies()
+    }
+
+    getMovies = async () => {
+        const Resposta = await FilmesApi.get()
+        console.log(Resposta)
+
+        const AllFilmes = Resposta.data.results.map((item) => {
+            return {
+                ...item,
+                nome: item.original_title
+            }
+        })
+        console.log(AllFilmes)
+        this.setState({
+            movies: AllFilmes
+        })
+    }
+
     render() {
         return (
             <>
-                
+                <ul>
+                    {this.state.movies.map((item, index) => (
+                        <ul key={index}>
+                            <li>{item.nome}</li>
+                            <li>{item.overview}</li>
+                            <li>{item.vote_average}</li>
+                        </ul>
+                    ))}
+                </ul>
             </>
         )
     }
